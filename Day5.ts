fafcf0e39bec7b30479cd4212fe94bd37c268170
Salisa <159696788+9900salisa@users.noreply.github.com>
2024-03-20 02:54:11 +0500

    Add files via upload

---
+++ Day5.ts
@@ -0,0 +1,5 @@
+let transportation:string[] =["Honda Motercycle","Honda city","Honda civics","BMW"]
+
+console.log(" I would like to own a "+ " "+transportation[0]);
+
+transportation.map((item)=> console.log("I would like to own a",item) );
