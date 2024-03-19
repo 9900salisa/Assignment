a83bab4828adffdcadd5a3f4b9fe40778494448b
Salisa <159696788+9900salisa@users.noreply.github.com>
2024-03-20 02:56:21 +0500

    Add files via upload

---
+++ Day3.ts
@@ -0,0 +1,33 @@
+// let a:number= 6;
+// let b:number= 2;
+
+// console.log(6+2);
+
+// let c:number = 10;
+// let d:number = 2;
+
+// console.log(c-d);
+
+//addition
+
+let addition:number = 6+2;
+
+console.log("Addition",addition);
+
+//subtraction
+
+let sub:number =10-2;
+
+console.log(`subtraction ${sub}`);
+
+//multiplication
+
+let multiplication:number = 4*2;
+
+console.log("multiplication", multiplication);
+
+//division
+
+let division:number =16/2;
+
+console.log("Division",division);
