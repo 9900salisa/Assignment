f119c67456f3a78dacb163a43f2b80826e7e0348
Salisa <159696788+9900salisa@users.noreply.github.com>
2024-03-20 02:49:49 +0500

    Add files via upload

---
+++ Day2.ts
@@ -0,0 +1,14 @@
+// console.log('Albert Einstein once said, “A person who never made a mistake never tried anything new.”')
+
+// let famouspersons: string = "Albert Einstein";
+// let message: string = `${famousperson} once said, “A person who never made a mistake never tried anything new.”`;
+
+// console.log(message);
+
+
+let fsname:string="salisa\tkhan";
+let fsname2:string="salisa\nkhan";
+
+console.log(fsname);
+console.log(fsname2);
+
